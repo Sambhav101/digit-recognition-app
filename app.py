9259b71f45184a66db4312fdefaa6863bffeff76
Sambhav Shrestha <sambhavshrestha1@gmail.com>
2021-03-17 21:13:36 -0400

    fixed port issue

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import json
+import os
 import numpy as np
 import cv2
 import re
@@ -62,4 +63,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
